3548ff630a89fd3b471be964286872a2b6bae286
Emma Campbell <ecampb10@u.rochester.edu>
2019-12-10 17:01:18 -0500

    journals front end- basic homepage just a filler homepage, doesn't take in user/journal entry data yet. but does have modal for delete button.

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import Register from '@/views/register/Register.vue';
+import Journals from '@/views/journals/Journals.vue';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register,
+    },
+    {
+      path: '/journal',
+      name: 'journal',
+      component: Journals,
     },
   ],
 });
---
+++ client/src/views/journals/Journals.vue
@@ -0,0 +1,13 @@
+<template src="./template.html"></template>
+<script lang="ts">
+import 'reflect-metadata';
+
+export default {
+  data() {
+    return {
+      isModalActive: false,
+    };
+  },
+};
+</script>
+<style src="../register/styles.scss" lang="scss"></style>
