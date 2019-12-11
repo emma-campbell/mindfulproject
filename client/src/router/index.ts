37fc5512d12b4355a1a6dca9e3b602a377e099da
Emma Campbell <ecampb10@u.rochester.edu>
2019-12-10 20:03:07 -0500

    habits front end css just a filler, its really bad

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import Register from '@/views/register/Register.vue';
+import Habits from '@/views/habits/Habits.vue';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register,
+    },
+    {
+      path: '/habits',
+      name: 'habits',
+      component: Habits,
     },
   ],
 });
---
+++ client/src/views/habits/Habits.vue
@@ -0,0 +1,33 @@
+<template src="./template.html"></template>
+<script lang="ts">
+import 'reflect-metadata';
+
+export default {
+  data() {
+    const data = [
+      {
+        habit: 'make bed',
+      },
+      {
+        habit: 'drink water',
+      },
+      {
+        habit: 'positive self talk',
+      },
+      {
+        habit: 'exercise',
+      },
+      {
+        habit: 'meditate',
+      },
+    ];
+
+    return {
+      data,
+      isEmpty: false,
+      isBordered: true,
+    };
+  },
+};
+</script>
+<style src="../register/styles.scss" lang="scss"></style>
